156981db71007c60190b76d2a6893abb828611cd
tramnguyengit <mylongqa@gmail.com>
2022-09-21 19:09:56 +0700

    Update lab_03.2 & la_03.3

--- src/lab_03/lab_03_02.java
+++ src/lab_03/lab_03_02.java
@@ -1,15 +1,32 @@
 package lab_03;
 
-import java.util.Arrays;
-
 public class lab_03_02 {
-/*    Finding maximum value/minimum value from an integer array
-    int[] intArr = {1, 2, 3, 4, 5};
-    Minimum: 1
-    Maximum: 5*/
+    /*    Finding maximum value/minimum value from an integer array
+        int[] intArr = {1, 2, 3, 4, 5};
+        Minimum: 1
+        Maximum: 5*/
+
     public static void main(String[] args) {
-        int[] arrayNum = {1, 2, 7, 100, 5};
-        System.out.println("Maximum value from the array is :" + Arrays.stream(arrayNum).max().getAsInt());
-        System.out.println("Maximum valye from the array is :" + Arrays.stream(arrayNum).min().getAsInt());
+        getMinMax();
+    }
+
+    private static void getMinMax() {
+        int[] arrayNum = {10, 2, 7, 100, 5};
+        int minNum = arrayNum[0];
+        int maxNum = 0;
+        for (int i : arrayNum) {
+            if (minNum > i) {
+                minNum = i;
+            }
+            if (maxNum < i) {
+                maxNum = i;
+            }
+        }
+        System.out.println("Minimum number in array is: " + minNum);
+        System.out.println("Maximum number in array is: " + maxNum);
     }
 }
+
+
+
+
