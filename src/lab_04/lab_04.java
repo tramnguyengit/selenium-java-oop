eb3c494930b9d5383fb66e87f0c644954367cf0a
tramnguyengit <mylongqa@gmail.com>
2022-09-21 22:27:35 +0700

    Add lab_04

---
+++ src/lab_04/lab_04.java
@@ -0,0 +1,41 @@
+package lab_04;
+
+import java.util.*;
+
+public class lab_04 {
+    public static void main(String[] args) {
+        List<Integer> myArrNum = new ArrayList<>();
+        System.out.println("Please enter several number");
+        Scanner scanner = new Scanner(System.in);
+        int firstNum = scanner.nextInt();
+        int secondNum = scanner.nextInt();
+        int thirdNum = scanner.nextInt();
+        int fourthNum = scanner.nextInt();
+        myArrNum.add(firstNum);
+        myArrNum.add(secondNum);
+        myArrNum.add(thirdNum);
+        myArrNum.add(fourthNum);
+        System.out.println("Your list Number is: " + myArrNum);
+        Set<Integer> set = new HashSet<>(myArrNum);
+        System.out.println("Your list after sorting: "+ set);
+        List<Integer> newArrNum = new ArrayList<>(set);
+        int length = newArrNum.size();
+        System.out.println("Minimum number is: " + newArrNum.get(0));
+        System.out.println("Minimum number is: " + newArrNum.get(length - 1));
+       findNum(newArrNum);
+
+    }
+    private static void findNum(List<Integer> newArrNum){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int searchNum = scanner.nextInt();
+        newArrNum.contains(searchNum);
+        if(newArrNum.contains(searchNum)){
+            System.out.println("The position of your number is: " + newArrNum.indexOf(searchNum));
+
+        } else {
+            System.out.println("There is no this number in list");
+        }
+    }
+
+}
