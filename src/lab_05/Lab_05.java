193870e805eb57eff367505f8e76150626266565
tramnguyengit <mylongqa@gmail.com>
2022-10-11 11:22:27 +0700

    Update labrunable

--- src/lab_05/Lab_05.java
+++ src/lab_05/Lab_05.java
@@ -1,70 +1,59 @@
 package lab_05;
 
+import lab_runable.Lab_05_Test;
+
 import java.util.*;
 
 public class Lab_05 {
 
-    public static void printGameMenu() {
-        System.out.println("== Game Menu");
-        System.out.println("1. Add number into ArrayList");
-        System.out.println("2. Print numbers");
-        System.out.println("3. Get maximum number");
-        System.out.println("4. Get minimum number");
-        System.out.println("5. Find Number");
-        System.out.println("6. Reset Array List");
-    }
-
-    public static int getUserOption() {
+    public static void main(String[] args) {
+        List<Integer> myArrList = new ArrayList<>();
+        System.out.println("How many number you want to enter?");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the option: ");
-        return scanner.nextInt();
-    }
+        int numberOfDigit = scanner.nextInt();
+        for (int i = 0; i < numberOfDigit; i++) {
+            System.out.println("Please enter the number");
+            int number = scanner.nextInt();
+            myArrList.add(number);
+        }
+        boolean isContinue = true;
+        while (isContinue) {
+            Lab_05_Test.printGameMenu();
+            int inputOption = Lab_05_Test.getUserOption();
+            switch (inputOption) {
+                case 0:
+                    isContinue = false;
+                    break;
+                case 1:
+                    Lab_05_Test.addNumber(myArrList);
+                    break;
+                case 2:
+                    Lab_05_Test.printArrayList(myArrList);
+                    break;
+                case 3:
+                    if(myArrList.size() > 0){
+                        int maxNumber = Lab_05_Test.getMaximumNumber(myArrList);
+                        System.out.println("Maximum Number is :"+ maxNumber);
+                    } else System.out.println("Array is blank");
+                    break;
+                case 4:
+                    if(myArrList.size() > 0){
+                        int minNum = Lab_05_Test.getMinimumNumber(myArrList);
+                        System.out.println("Minimum Number is :"+ minNum);
+                    } else System.out.println("Array is blank");
 
-    public static void addNumber(List<Integer> myArrList) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the number: ");
-        int inputNumber = scanner.nextInt();
-        myArrList.add(inputNumber);
-    }
+                    break;
+                case 5:
+                    Lab_05_Test.findNumber(myArrList);
+                    break;
+                case 6:
+                    Lab_05_Test.resetArrayList(myArrList);
+                    System.out.println("Your Original List: "+ myArrList);
+                default:
+                    System.out.println("Incorrect option");
 
-    public static void printArrayList(List<Integer> myArrList) {
-        System.out.println("Your Array List is: " + myArrList);
-    }
-
-    public static int getMaximumNumber(List<Integer> myArrlist) {
-        int maxNumber = 0;
-        for (int i = 0; i < myArrlist.size() - 1; i++) {
-            if(myArrlist.get(i) < myArrlist.get(i+1)){
-                maxNumber= myArrlist.get(i+1);
             }
         }
-        return maxNumber;
     }
 
-    public static int getMinimumNumber(List<Integer> myArrlist) {
-        int minNum = 0;
-        for (int i = 0; i < myArrlist.size() - 1; i++) {
-            if(myArrlist.get(i) < myArrlist.get(i+1)){
-                minNum = myArrlist.get(i);
-            }
-        }
-        return minNum;
-    }
-
-    public static void resetArrayList(List<Integer> myArrList) {
-        myArrList.clear();
-    }
-
-    public static void findNumber(List<Integer> newArrNum) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the number: ");
-        int searchNum = scanner.nextInt();
-        newArrNum.contains(searchNum);
-        if (newArrNum.contains(searchNum)) {
-            System.out.println("The position of your number is: " + newArrNum.indexOf(searchNum));
-
-        } else {
-            System.out.println("There is no this number in list");
-        }
-    }
 }
