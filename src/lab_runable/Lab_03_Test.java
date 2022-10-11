95ff07f92f7767635b82566827d599fb00b11a76
tramnguyengit <mylongqa@gmail.com>
2022-10-11 11:25:03 +0700

    Update labrunable

--- src/lab_runable/Lab_03_Test.java
+++ src/lab_runable/Lab_03_Test.java
@@ -1,2 +1,68 @@
-package lab_runable;public class Lab_03_Test {
+package lab_runable;
+
+import java.util.Arrays;
+
+public class Lab_03_Test {
+
+    public static void countOddAndEvenNum(int[] arrayNum, int totalEvenNum){
+        for (int index = 0; index < 5; index++) {
+            if (arrayNum[index] % 2 == 0) {
+                totalEvenNum++;
+            }
+        }
+        System.out.println("Total even number is: " + totalEvenNum);
+        int totalOddNum = arrayNum.length - totalEvenNum;
+        System.out.println("Total odd number in the array is: " + totalOddNum);
+    }
+
+    public static void getMinMax() {
+        int[] arrayNum = {10, 2, 7, 100, 5};
+        int minNum = arrayNum[0];
+        int maxNum = 0;
+        for (int i : arrayNum) {
+            if (minNum > i) {
+                minNum = i;
+            }
+            if (maxNum < i) {
+                maxNum = i;
+            }
+        }
+        System.out.println("Minimum number in array is: " + minNum);
+        System.out.println("Maximum number in array is: " + maxNum);
+    }
+
+    public static void sortArray(int[] anArray){
+        for (int i = 0; i < anArray.length; i++) {
+            for (int j = i+1; j < anArray.length; j++) {
+                if (anArray[i] > anArray[j]) {
+                    int originalNum = anArray[i];
+                    anArray[i] = anArray[j];
+                    anArray[j] = originalNum;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(anArray));
+    }
+
+    public static int[] mergeMethod(int[] firstArr, int[] secondArr) {
+        int arrLength = firstArr.length + secondArr.length;
+        int[] mergedArr = new int[arrLength];
+        int firstArrPo, secondArrPo, mergedArrPo;
+        firstArrPo = secondArrPo = mergedArrPo = 0;
+        while (firstArrPo < firstArr.length && secondArrPo < secondArr.length) {
+            if (firstArr[firstArrPo] < secondArr[secondArrPo]) {
+                mergedArr[mergedArrPo++] = firstArr[firstArrPo++];
+            } else {
+                mergedArr[mergedArrPo++] = secondArr[secondArrPo++];
+            }
+        }
+        while (firstArrPo < firstArr.length) {
+            mergedArr[mergedArrPo++] = firstArr[firstArrPo++];
+        }
+        while (secondArrPo < secondArr.length) {
+            mergedArr[mergedArrPo++] = secondArr[secondArrPo++];
+
+        }
+        return mergedArr;
+    }
 }
--- src/lab_runable/Lab_05_Test.java
+++ src/lab_runable/Lab_05_Test.java
@@ -1,2 +1,72 @@
-package lab_runable;public class Lab_05_Test {
+package lab_runable;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Lab_05_Test {
+
+    public static void printGameMenu() {
+        System.out.println("== Game Menu");
+        System.out.println("1. Add number into ArrayList");
+        System.out.println("2. Print numbers");
+        System.out.println("3. Get maximum number");
+        System.out.println("4. Get minimum number");
+        System.out.println("5. Find Number");
+        System.out.println("6. Reset Array List");
+    }
+
+    public static int getUserOption() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the option: ");
+        return scanner.nextInt();
+
+    }
+
+    public static void addNumber(List<Integer> myArrList) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int inputNumber = scanner.nextInt();
+        myArrList.add(inputNumber);
+    }
+
+    public static void printArrayList(List<Integer> myArrList) {
+        System.out.println("Your Array List is: " + myArrList);
+    }
+
+    public static int getMaximumNumber(List<Integer> myArrlist) {
+        int maxNumber = 0;
+        for (int i = 0; i < myArrlist.size() - 1; i++) {
+            if(myArrlist.get(i) < myArrlist.get(i+1)){
+                maxNumber= myArrlist.get(i+1);
+            }
+        }
+        return maxNumber;
+    }
+
+    public static int getMinimumNumber(List<Integer> myArrlist) {
+        int minNum = 0;
+        for (int i = 0; i < myArrlist.size() - 1; i++) {
+            if(myArrlist.get(i) < myArrlist.get(i+1)){
+                minNum = myArrlist.get(i);
+            }
+        }
+        return minNum;
+    }
+
+    public static void resetArrayList(List<Integer> myArrList) {
+        myArrList.clear();
+    }
+
+    public static void findNumber(List<Integer> newArrNum) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int searchNum = scanner.nextInt();
+        newArrNum.contains(searchNum);
+        if (newArrNum.contains(searchNum)) {
+            System.out.println("The position of your number is: " + newArrNum.indexOf(searchNum));
+
+        } else {
+            System.out.println("There is no this number in list");
+        }
+    }
 }
